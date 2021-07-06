fe377ef7d2c2c860b7b3c1439c5d2aa50ed7ac0e
pororobro <pororobro@naver.com>
2021-07-06 10:53:35 +0900

    master

--- frontend/src/member/components/MemberModifyForm.js
+++ frontend/src/member/components/MemberModifyForm.js
@@ -2,47 +2,36 @@ import React,{useEffect, useState} from 'react'
 import 'member/styles/MemberDetail.css'
 import { memberModify } from 'api'
 const MemberModifyForm = () => {
-    const [member, setMember] = useState({})
+    const [changedPassword, setChangedPassword] = useState('')
 
-
-    useEffect(() => {
-      setMember(JSON.parse(localStorage.getItem("selectedMember")))
-    }, {})
 
     const handleSubmit = e => {
       e.preventDefault()
+      const member = JSON.parse(localStorage.getItem("loginedMember"))
+      alert(changedPassword)
+      member.password = changedPassword
       alert(JSON.stringify(member))
-      member.password = e.target.value
-      alert(JSON.stringify(member))
+      
       memberModify({member})
       .then(res => {
-        alert(`회원가입 완료 : ${res.data.result} `)
+        alert(`비밀번호 수정 완료 : ${res.data.result} `)
+        localStorage.setItem("loginedMember", res.data.result)
         // history.push('login')
         
       })
       .catch(err => {
-        alert(`회원가입 실패 : ${err} `)
+        alert(`비밀번호 수정 실패 : ${err} `)
   
       })
     }
 
     return (<>
-             <div className="member-detail-card">
-                <h2 style={{"text-align":"center"}}>상세정보 수정</h2>
-                <img src="https://www.w3schools.com/w3images/team2.jpg"  style={{"width":"100%"}}/>
-                <h1>{member.name}</h1>
-                    <p className="member-detail-title">CEO & Founder, Example</p>
-                    <p>Harvard University</p>
-                    <div style={{"margin": "24px 0"}}>
-
-
-
-                    <form method="post" onSubmit={handleSubmit} >
-         
-
+    <form method="put" onSubmit={handleSubmit} >
+            
+                <h2 style={{"text-align":"center"}}>비밀번호 수정</h2>
         <div className="container">
           <label labelFor="psw"><b>변경할 비밀번호</b></label>
-          <input type="password" placeholder="Enter Password" name="password" required/>
+          <input type="password" placeholder="Enter Password" onChange={e => {setChangedPassword(e.target.value)}} name="password" required/>
               
           <button type="submit">확 인</button>
          
@@ -50,14 +39,6 @@ const MemberModifyForm = () => {
 
       </form>
 
-
-
-
-
-                    </div>
-                    <p><button className="member-detail-button">Contact</button></p>
-                </div>
-                
        
       </>)
 }
--- frontend/src/member/index.js
+++ frontend/src/member/index.js
@@ -6,10 +6,9 @@ export { default as MemberModifyForm } from 'member/components/MemberModifyForm'
 export { default as MemberNav } from 'member/components/MemberNav'
 export { default as MemberRegisterForm } from 'member/components/MemberRegisterForm'
 export { default as MemberRetrieveForm } from 'member/components/MemberRetrieveForm'
+export { default as MemberDelete } from 'member/pages/MemberDelete'
 export { default as MemberDetail } from 'member/pages/MemberDetail'
 export { default as MemberList } from 'member/pages/MemberList'
 export { default as MemberLogin } from 'member/pages/MemberLogin'
 export { default as MemberModify } from 'member/pages/MemberModify'
 export { default as MemberRegister } from 'member/pages/MemberRegister'
-
-
